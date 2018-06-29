4e0f1c30f5efac52979317c8ac55eea782031bf6
YuriiSH <yurashmorgun@gmail.com>
2018-06-29 22:12:24 +0300

    First working programe (too big controller)

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-/* Task1.java   0.1   28.06.18
+/* Task1.java   1   28.06.18
  *
  * Copyright (c) Open source project
  *
@@ -7,52 +7,24 @@
  * You haven`t change the code if you are not a trainer
  */
 
+
 import core.controller.Controller;
 import core.model.Model;
-import core.view.EMessage;
 import core.view.View;
-
-
-import java.util.ArrayList;
 
 /*
  * This is the main class for running program
  *
- * @version     0.1     29.06.2018
+ * @version     1     29.06.2018
  * @author      Yurii Shmorgun
 */
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
-
-        /*String first = "Hi all";
-        String second = "My name is Yurii";
-        ArrayList<String> arrayList = new ArrayList();
-        arrayList.add(first);
-        arrayList.add(second);
-
-        Model model = new Model();
-        model.summationValues(arrayList);
-        System.out.println(model.getResultForUserString());
-*/
-
-       /* System.out.println(EMessage.INPUT_ERROR.getMesage());*/
-/*
-
-        View view = new View();
-        view.printMessage(EMessage.INPUT_WORD_HELLO);
-*/
-
 
     Model model = new Model();
     View view = new View();
     Controller controller = new Controller(model, view);
     controller.processUser();
-
-
-
-
-
     }
 }
--- src/core/controller/Controller.java
+++ src/core/controller/Controller.java
@@ -1,29 +1,42 @@
+/* Task1.java   0.1   28.06.18
+ *
+ * Copyright (c) Open source project
+ *
+ * This software use for home task1 and learn book clear code
+ * You can use any material for your oven project
+ * You haven`t change the code if you are not a trainer
+ */
+
 package core.controller;
 
 import core.model.Model;
 import core.view.EMessage;
 import core.view.View;
-
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/**
- * Created by Yurii on 29.06.2018.
- */
+/*
+ * This is the controller
+ *
+ * @version     1     29.06.2018
+ * @author      Yurii Shmorgun
+*/
 public class Controller {
 
-
-
+    /*create classes for */
     private Model model;
     private View view;
+
+    /*Create List for collecting Strings from input*/
     private ArrayList<String> stringList = new ArrayList<>();
 
+    /*constructor*/
     public Controller(Model model, View view) {
         this.model = model;
         this.view = view;
     }
 
+    /*main methods*/
     public void processUser(){
         Scanner scanner = new Scanner(System.in);
 
@@ -37,11 +50,13 @@ public class Controller {
         view.printControllerMessage(model.getResultForUserString());
     }
 
+    /*Check methods for input Strings*/
     private String inputStringFromScanner (String string, Scanner scanner, EMessage eMessage){
       view.printMessage(eMessage);
 
         while( ! scanner.hasNext(string)){
-            view.printMessage(EMessage.INPUT_ERROR);
+            view.printMessage(EMessage.INPUT_ERROR );
+            view.printMessage(eMessage);
             scanner.next();
         }
         return scanner.next();
